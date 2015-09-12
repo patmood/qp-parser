81fe4fb00ddf93ff0bbfd3ae73c798e2c1cc80ea
patmood <patrick.n.moody@gmail.com>
2015-09-12 14:16:39 -0700

    more test setup

--- __tests__/parse-test.js
+++ __tests__/parse-test.js
@@ -1,8 +1,36 @@
-jest.dontMock('../libs/parse');
+'use strict'
+jest.autoMockOff()
+const parse = require('../libs/parse')
 
-describe('parse', function() {
-  it('does anything', function() {
-    const parse = require('../libs/parse')
-    expect(parse(4)).toBe(3)
+let debugStore = []
+const debug = (label, arg) => {
+  debugStore.push([label, arg])
+}
+
+describe('parse', () => {
+  beforeEach(() => {
+    debugStore = []
+  })
+
+  it('skips empty input', () => {
+    const given = ''
+    const expectedReturn = {}
+    const expectedDebug = []
+
+    const actual = parse(given, debug)
+    expect(actual).toEqual(expectedReturn)
+    expect(debugStore).toEqual(expectedDebug)
+  })
+
+  it('single un-nested pair', () => {
+    const given = 'a=b'
+    const expectedReturn = {a: 'b'}
+    const expectedDebug = [
+      ['delimited pairs', ['a=b']]
+    ]
+
+    const actual = parse(given, debug)
+    expect(actual).toEqual(expectedReturn)
+    expect(debugStore).toEqual(expectedDebug)
   })
 })
--- libs/parse.js
+++ libs/parse.js
@@ -1,5 +1,15 @@
-const parse = function(qs) {
-  return qs
+'use strict'
+const noop = () => {}
+
+const parse = function(qs, debug) {
+  if (!qs) return {}
+  let pairs = qs.split('&')
+  debug('delimited pairs', pairs)
+
+  let qsList = qs.split('=')
+  let qsObj = {}
+  qsObj[qsList[0]] = qsList[1]
+  return qsObj
 }
 
 module.exports = parse
